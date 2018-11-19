39cf9493ecd7c3874cdb88531a52642412b401d2
davidasrobot <yogasakajr@gmail.com>
2018-11-19 12:43:31 +0700

    card and catergory

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'categories/index'
   devise_for :admins
   devise_for :users
 
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
     resources :forum_posts
     resources :replies_posts    
   end
+  resources :categories , only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
