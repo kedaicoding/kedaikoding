7af691d7d5c358474db859cda51aa52e56554fcc
davidasrobot <yogasakajr@gmail.com>
2018-12-12 13:50:41 +0700

    adding user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
+
     def show 
         @user = User.find(params['id'])
     end
-    
+
+    def edit
+        @user = User.find(params[:id])
+        authorize @user
+    end
+
+    def update
+        @user = User.find(params[:id])
+        authorize @user
+        if @user.update(useredit_params)
+            redirect_to user_path
+        else
+            render 'edit'
+        end
+    end
+
+    private
+
+    def useredit_params
+        params.require(:users).permit(:full_name, :phone_number, :email)
+    end
 end
